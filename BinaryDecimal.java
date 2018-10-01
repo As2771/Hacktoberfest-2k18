51bbe370e25f3e48f91b90a6d8c6ec67fb08be44
siddhartthecoder <siddharth1997saurabh@gmail.com>
2018-10-01 23:12:12 +0530

    added a java program to convert binary to decimal

---
+++ BinaryDecimal.java
@@ -0,0 +1,27 @@
+public class BinaryDecimal {
+ 
+  public int BinaryToDecimal(int binaryNumber){
+ 
+    int decimal = 0;
+    int p = 0;
+    while(true){
+      if(binaryNumber == 0){
+        break;
+      } else {
+          int temp = binaryNumber%10;
+          decimal += temp*Math.pow(2, p);
+          binaryNumber = binaryNumber/10;
+          p++;
+       }
+    }
+    return decimal;
+  }
+ 
+  public static void main(String args[]){
+    BinaryDecimal obj = new BinaryDecimal();
+    System.out.println("110 --> "+obj.BinaryToDecimal(110));
+    System.out.println("1101 --> "+obj.BinaryToDecimal(1101));
+    System.out.println("100 --> "+obj.BinaryToDecimal(100));
+    System.out.println("110111 --> "+obj.BinaryToDecimal(110111));
+  }
+}
