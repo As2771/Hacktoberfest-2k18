b7fdc29f3a094d325f4f89ec916216143290a552
ApoorvAgarwal21 <32730652+ApoorvAgarwal21@users.noreply.github.com>
2018-10-02 01:22:18 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,10 @@
+//to calculate factorial of a number
+#include<stdio.h>
+int main() {
+    int i,n,x=1;
+    printf("enter the value for calculating factorial\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {x = x*i;}
+    printf("value of %d! is %d",n,x);
+}
