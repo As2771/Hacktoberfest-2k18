92432faabc3c4f6a46bc16328e167eb518b0d515
abcdefgh12 <43812657+abcdefgh12@users.noreply.github.com>
2018-10-03 16:57:30 +0530

    concat2num.py

---
+++ concat2num.py
@@ -0,0 +1,5 @@
+a=input("Enter number 1")       # input number 1 ex. 5
+
+b=input("Enter number 2")       # input number 2 ex. 7
+
+print (a+b)                     # output will be 57 because data type of input function is string by default so when we add strings they get concatenated
