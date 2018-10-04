2b80c030314dfe81d409cf0431b45fdea459098a
Shweta Goyal <36664302+Shwetago@users.noreply.github.com>
2018-10-04 13:13:36 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,5 @@
-Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+Python 3.6.5 (v3.6.5:f59c0932b4) on win32
+
 >>> def fib(n):
 	if(n == 0):
 		return 0
