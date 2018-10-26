0360c0fd14fae8de37404a5d7af44967a9652f71
njain794 <44344547+njain794@users.noreply.github.com>
2018-10-26 10:57:58 +0530

    Update factorial_in_c.c
    though your code was working and giving output but I AM SURE MY PIECE OF CODE WILL APPEAL YOU MORE
    THANKYOU !!!!!!!!!!!!!!!!!!!

--- factorial_in_c.c
+++ factorial_in_c.c
@@ -1,10 +1,21 @@
 //to calculate factorial of a number
 #include<stdio.h>
-int main() {
+#include<conio.h>
+void main()
+{   
     int i,n,x=1;
-    printf("enter the value for calculating factorial\n");
+    printf("enter the value for calculating factorial = \n");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
-    {x = x*i;}
-    printf("value of %d! is %d",n,x);
+    {    
+       x=x*i;
+        if(n==i)
+        {
+          printf("%d=",i);
+        }
+        else
+        {    
+        printf("%d*",i);
+    }    
+       getch(); 
 }
